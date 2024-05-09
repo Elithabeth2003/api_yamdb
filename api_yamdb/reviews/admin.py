8bdbfb78d0bf9ab7fd97c455c8b37021f1258286
Николай Круглов <krynik94@gmail.com>
2024-05-09 18:00:37 +0300

    Удобный обзор в админке

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,8 +3,30 @@ from django.contrib import admin
 from .models import Category, Comment, Genre, Title, Review
 
 
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Genre)
-admin.site.register(Title)
-admin.site.register(Review)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'score', 'pub_date')
+    search_fields = ('author','score',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'pub_date', 'review',)
