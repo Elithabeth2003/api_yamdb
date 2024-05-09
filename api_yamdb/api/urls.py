c4f4340ed322d5f17356afbfca7e54972bc5f5aa
Николай Круглов <krynik94@gmail.com>
2024-05-09 17:05:22 +0300

    Добавил урлы на отзывы/коменты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,17 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 
-from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet, CommentViewSet, ReviewViewSet
 
 
 router_v1 = DefaultRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 v1_patterns = [
     path('', include(router_v1.urls)),
