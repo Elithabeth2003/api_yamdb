729cdbbcb7ba342c68c1224f454dfb92177bf98f
nepa27 <nepalek@mail.ru>
2024-05-09 16:01:31 +0300

    Удалил лишние файлы, заполнил модель, создал файл для сериализоторов

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+ROLE_CHOICES = [
+    ('user', 'User'),
+    ('moderator', 'Moderator'),
+    ('admin', 'Admin'),
+]
+
+# добавил пока себе тоже, предлагаю отдельный файл для констант добавить
+LENGTH_OF_NAME = 30
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150
+
+    )
+    email = models.EmailField(
+        verbose_name='Email address',
+        max_length=254
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+        blank=True
+    )
+    bio = models.TextField(
+        verbose_name='О себе',
+        blank=True
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        default=ROLE_CHOICES[0],
+        max_length=10,
+        choices=ROLE_CHOICES
+    )
+
+    def __str__(self):
+        return self.username[:LENGTH_OF_NAME]
+
