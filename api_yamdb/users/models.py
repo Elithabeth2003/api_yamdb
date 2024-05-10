609bc102350cf291775d1cb80bb5576df1754051
nepa27 <nepalek@mail.ru>
2024-05-10 07:02:56 +0300

    Запихнул всё в один коммит, прошу прощения; Добавил пару строк в настройки; Добавил импорт в общий урл; Поправил модель, добавил property; Создал файл пермишн; Сериализаторы создал и заполнил; Тоже с вьюшками; Создал файл с урлами

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,24 +1,31 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-ROLE_CHOICES = [
-    ('user', 'User'),
-    ('moderator', 'Moderator'),
-    ('admin', 'Admin'),
-]
 
 # добавил пока себе тоже, предлагаю отдельный файл для констант добавить
 LENGTH_OF_NAME = 30
 
 
 class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+    ]
+
     username = models.CharField(
         verbose_name='Имя пользователя',
+        unique=True,
         max_length=150
 
     )
     email = models.EmailField(
         verbose_name='Email address',
+        unique=True,
         max_length=254
     )
     first_name = models.CharField(
@@ -37,11 +44,22 @@ class User(AbstractUser):
     )
     role = models.CharField(
         verbose_name='Роль',
-        default=ROLE_CHOICES[0],
+        default='user',
         max_length=10,
         choices=ROLE_CHOICES
     )
 
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    @property
+    def is_user(self):
+        return self.role == self.USER
+
     def __str__(self):
         return self.username[:LENGTH_OF_NAME]
-
